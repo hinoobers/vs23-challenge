35e957a2039ee81dbc82606a8f920462d439c996
reio <reio.poldmaa@voco.ee>
2025-01-13 12:56:06 +0200

    Ülesanne 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Header from "./components/Header"; 
+import Meals from "./components/Meals";
+
 const App = () => {
  return (
     <>
-     <h1>Food Order App</h1>
+    <Header>
+      <h1>Food Order App</h1>
+    </Header>
+    <Meals></Meals>
     </>
   );
 }
