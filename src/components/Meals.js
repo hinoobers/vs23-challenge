190ac76d7995d36598f7774d20e6b6caff091af5
reio <reio.poldmaa@voco.ee>
2025-01-13 13:02:26 +0200

    Ülesanne 3 - andmete pärimine Meals.js

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,9 +1,17 @@
 const Meals = () => {
+    const meals = [];
+    fetch("http://localhost:3001/meals")
+        .then((response) => response.json())
+        .then((data) => {
+            const arrayobj = data;
+
+            for(let i = 0; i < arrayobj.length; i++) {
+                console.log(arrayobj[i]);
+            }
+        })
+
     return (
         <ul id="meals">
-            { 
-                // list of meals
-            }
         </ul>
     )
 }
