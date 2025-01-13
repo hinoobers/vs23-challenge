812cba161c1b9cc413f9ea87cc83e704d45b1d98
reio <reio.poldmaa@voco.ee>
2025-01-13 13:49:27 +0200

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Header from "./components/Header"; 
 import Meals from "./components/Meals";
+import Modal from "./components/UI/Modal";
 import { CartProvider } from "./store/CartContext";
+import ReactDOM from "react-dom";
+import { useState } from "react";
 
 const App = () => {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -9,8 +12,9 @@ const App = () => {
     setCartIsShown(!cartIsShown);
   }
 
- return (
+  return (
     <CartProvider>
+      {ReactDOM.createPortal(<Modal toggle={cartIsShown}></Modal>)}
     <Header modal={showCartHandler}>
       <h1>Food Order App</h1>
     </Header>
