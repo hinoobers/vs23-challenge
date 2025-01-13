4461a6c2224fc256584031bd3bfd8411076a54be
reio <reio.poldmaa@voco.ee>
2025-01-13 13:09:28 +0200

    Ülesanne 5 - valuuta näitamine

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,18 @@
 const MealItem = (props) => {
+
+    const format = (price) => {
+        return new Intl.NumberFormat('et-EE', {
+            style: 'currency',
+            currency: 'EUR'
+        }).format(price)
+    }
     return (
         <li>
             <article>
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p>props.meal.price</p>
+                    <p>{format(props.meal.price)}</p>
                     <p>{props.meal.description}</p>
                 </div>
                 <p>
